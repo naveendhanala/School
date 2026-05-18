c2af2380b7944ae50f288af5761d79830d9121cb
unknown <naveendhanala@gmail.com>
2026-05-18 11:57:47 +0530

    feat: extract HEAD_LABELS and MODE_LABELS to shared constants

---
+++ lib/constants/labels.ts
@@ -0,0 +1,19 @@
+// lib/constants/labels.ts
+export const HEAD_LABELS: Record<string, string> = {
+  tuition: 'Tuition',
+  book: 'Book',
+  transport: 'Transport',
+  hostel: 'Hostel',
+  admission: 'Admission',
+  uniform: 'Uniform',
+  exam: 'Exam',
+  other: 'Other',
+}
+
+export const MODE_LABELS: Record<string, string> = {
+  cash: 'Cash',
+  upi: 'UPI',
+  cheque: 'Cheque',
+  neft_rtgs: 'NEFT/RTGS',
+  demand_draft: 'DD',
+}
